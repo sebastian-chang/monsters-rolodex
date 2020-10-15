615ecc9b2e5158439affc203a997f3ab2ba190b1
Sebastian <sebastian.chang23@gmail.com>
2020-10-14 22:26:46 -0500

    Updates

---
+++ precache-manifest.175d1850234f5b80eb1b15b34effbee2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "501ae04d9fc86b9e0ac8d2aaf7cc29ca",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "61528bde7b901aeb9b32",
+    "url": "/monsters-rolodex/static/css/main.14d30467.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61528bde7b901aeb9b32",
+    "url": "/monsters-rolodex/static/js/main.5d1b945e.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
